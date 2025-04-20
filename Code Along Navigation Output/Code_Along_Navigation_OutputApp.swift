0e4f2b3b9734d6a4ba242662e960c3429db2163c
Shira Stein <sstein@jkha.org>
2025-04-20 09:57:08 -0400

    Initial Commit

---
+++ Code Along Navigation Output/Code_Along_Navigation_OutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_Along_Navigation_OutputApp.swift
+//  Code Along Navigation Output
+//
+//  Created by Shira Stein on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_Along_Navigation_OutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code Along Navigation OutputTests/Code_Along_Navigation_OutputTests.swift
@@ -0,0 +1,17 @@
+//
+//  Code_Along_Navigation_OutputTests.swift
+//  Code Along Navigation OutputTests
+//
+//  Created by Shira Stein on 4/20/25.
+//
+
+import Testing
+@testable import Code_Along_Navigation_Output
+
+struct Code_Along_Navigation_OutputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
